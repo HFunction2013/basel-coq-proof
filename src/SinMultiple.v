c29548b2ef231f410ee92730460303b335d31417
HFunction2013 <HFunction2013@users.noreply.github.com>
2026-09-12 23:30:54 +0800

    Fill proof for Q_R_is_poly using mutual induction

--- src/SinMultiple.v
+++ src/SinMultiple.v
@@ -71,7 +71,40 @@ Qed.
 (** ** Q_n, R_n 都是多项式 *)
 
 Lemma Q_R_is_poly : forall (n : nat), is_poly n (Q n) /\ is_poly n (Rpoly n).
-Proof. Admitted.
+Proof.
+  intro n. induction n.
+  - split.
+    + simpl. apply is_poly_const.
+    + simpl. apply is_poly_const.
+  - destruct IHn as [HQ HR].
+    split.
+    + simpl.
+      assert (H1 : is_poly (S n) (fun y : R => (1 - 2*y) * Q n y)).
+      { assert (H1a : is_poly (S n) (fun y : R => Q n y)) by (apply is_poly_weaken with (m := n) (n := 1); exact HQ).
+        assert (H1b : is_poly (S n) (fun y : R => 2*y * Q n y)).
+        { assert (H1b1 : is_poly (S n) (fun y : R => y * Q n y)) by (apply is_poly_y_mult; exact HQ).
+          apply is_poly_scale with (c := 2); exact H1b1. }
+        apply is_poly_minus; [exact H1a | exact H1b]. }
+      assert (H2 : is_poly (S n) (fun y : R => 2*(1-y) * Rpoly n y)).
+      { assert (H2a : is_poly (S n) (fun y : R => Rpoly n y)) by (apply is_poly_weaken with (m := n) (n := 1); exact HR).
+        assert (H2b : is_poly (S n) (fun y : R => 2*y * Rpoly n y)).
+        { assert (H2b1 : is_poly (S n) (fun y : R => y * Rpoly n y)) by (apply is_poly_y_mult; exact HR).
+          apply is_poly_scale with (c := 2); exact H2b1. }
+        assert (H2c : is_poly (S n) (fun y : R => (1-y) * Rpoly n y)) by (apply is_poly_minus; [exact H2a | exact H2b]).
+        apply is_poly_scale with (c := 2); exact H2c. }
+      apply is_poly_plus; [exact H1 | exact H2].
+    + simpl.
+      assert (H3 : is_poly (S n) (fun y : R => (1 - 2*y) * Rpoly n y)).
+      { assert (H3a : is_poly (S n) (fun y : R => Rpoly n y)) by (apply is_poly_weaken with (m := n) (n := 1); exact HR).
+        assert (H3b : is_poly (S n) (fun y : R => 2*y * Rpoly n y)).
+        { assert (H3b1 : is_poly (S n) (fun y : R => y * Rpoly n y)) by (apply is_poly_y_mult; exact HR).
+          apply is_poly_scale with (c := 2); exact H3b1. }
+        apply is_poly_minus; [exact H3a | exact H3b]. }
+      assert (H4 : is_poly (S n) (fun y : R => 2*y * Q n y)).
+      { assert (H4a : is_poly (S n) (fun y : R => y * Q n y)) by (apply is_poly_y_mult; exact HQ).
+        apply is_poly_scale with (c := 2); exact H4a. }
+      apply is_poly_minus; [exact H3 | exact H4].
+Qed.
 
 Corollary Q_is_poly : forall (n : nat), is_poly n (Q n).
 Proof.
