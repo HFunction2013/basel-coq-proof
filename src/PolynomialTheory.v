9251c2f75d7910b31c8a2bb72e8684996387579b
HFunction2013 <HFunction2013@users.noreply.github.com>
2026-09-12 23:06:32 +0800

    Fill proof for geo_sum_factor using simpl pow + ring

--- src/PolynomialTheory.v
+++ src/PolynomialTheory.v
@@ -119,7 +119,16 @@ Fixpoint geo_sum (r y : R) (n : nat) : R :=
 
 Lemma geo_sum_factor : forall r y n,
     y^(S n) - r^(S n) = (y - r) * geo_sum r y n.
-Proof. Admitted.
+Proof.
+  intros r y n. induction n.
+  - simpl. ring.
+  - assert (H1 : (y - r) * geo_sum r y (S n) = y * ((y - r) * geo_sum r y n) + (y - r) * r^(S n)).
+    { simpl. ring. }
+    rewrite H1.
+    rewrite <- IHn.
+    simpl pow.
+    ring.
+Qed.
 
 (** geo_sum r y n 是 y 的 n 次多项式 *)
 Lemma geo_sum_is_poly : forall r n, is_poly n (fun y => geo_sum r y n).
